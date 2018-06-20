2f6b5f05def5d4fe0f31169d40a756f255e214ee
Aidan Dunne <aidandunne1892@gmail.com>
2018-06-20 19:40:45 +0100

    Post request add & Peer to Peer server Discused

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HHTP_PORT || 3001;
@@ -7,10 +8,20 @@ const app = express();
 const bc = new Blockchain();
 
 
+app.use(bodyParser.json());
+
 app.get('/blocks' ,(req, res) => {
      
     res.json(bc.chain);
 
+});
+
+app.post('/mine' , (req, res) => {
+
+    const block = bc.addBlock(req.body.data);
+    console.log(`New Block has been added : ${block.toString()}`);
+
+    res.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT , () => console.log(`Listening on port ${HTTP_PORT}`));
