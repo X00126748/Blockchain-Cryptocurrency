617be2c9b66f439d42da321789b3b0eeae252585
Aidan Dunne <aidandunne1892@gmail.com>
2018-06-27 23:49:44 +0100

    Websocket & peer-to-peer server created and tested, Next step is to add blockchain into the server

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pServer = require('./p2p-server');
 
-const HTTP_PORT = process.env.HHTP_PORT || 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
-
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -25,3 +26,4 @@ app.post('/mine' , (req, res) => {
 });
 
 app.listen(HTTP_PORT , () => console.log(`Listening on port ${HTTP_PORT}`));
+p2pServer.listen();
