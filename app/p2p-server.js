330a6625671841f99a5947c5dd59337526c6c1f9
Aidan Dunne <aidandunne1892@gmail.com>
2018-07-02 23:04:10 +0100

    Block chain message added to websocket once new websocket is started

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -35,7 +35,20 @@ connectToPeers(){
 connectSocket(socket){
     this.sockets.push(socket);
     console.log('Socket connected');
+
+    this.messageHandler(socket);
+    socket.send(JSON.stringify(this.blockchain.chain));
 }
+
+
+messageHandler(socket){
+    socket.on('message' , message => {
+        const data = JSON.parse(message);
+        console.log('data',data);
+    });
+}
+
+
 
 }
 
