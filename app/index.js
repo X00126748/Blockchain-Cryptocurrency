3358c03fd0f3e9f2b0e9fbac02afee9db4154f09
Aidan Dunne <aidandunne1892@gmail.com>
2018-07-24 21:07:00 +0100

    Transaction pool added to p2p server fully functional

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ const app = express();
 const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
-const p2pServer = new P2pServer(bc);
+const p2pServer = new P2pServer(bc, tp);
 
 app.use(bodyParser.json());
 
@@ -36,6 +36,18 @@ app.post('/mine' , (req, res) => {
 
 app.get('/transactions', (req,res) => {
     res.json(tp.transactions);
+});
+
+app.post('/transact', (req,res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient,amount,tp);
+    p2pServer.broadcastTransaction(transaction);
+    res.redirect('/transactions');
+});
+
+app.get('/public-key', (req,res) => {
+    res.json({ publicKey: wallet.publicKey });
+
 });
 
 app.listen(HTTP_PORT , () => console.log(`Listening on port ${HTTP_PORT}`));
